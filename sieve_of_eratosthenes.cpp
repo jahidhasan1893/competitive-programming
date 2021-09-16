7a687ac9b4ee917e8f1982c80ccb40b6b2bafe82
jahidhasan1893 <78349772+jahidhasan1893@users.noreply.github.com>
2021-09-16 23:20:34 +0600

    Create sieve_of_eratosthenes.cpp

---
+++ sieve_of_eratosthenes.cpp
@@ -0,0 +1,30 @@
+//printing all primes upto n
+
+#include<bits/stdc++.h>
+using namespace std;
+void sieve_of_eratosthenes(bool primes[],int n)
+{
+    for(int i=2;i*i<=n;i++)
+    {
+        if(primes[i]==true)
+        {
+            for(int j=i*i;j<=n;j+=i)
+            {
+                primes[j]=false;
+            }
+        }
+    }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    bool primes[n+1];
+    memset(primes,true,sizeof(primes));
+    sieve_of_eratosthenes(primes,n);
+    for(int i=2;i<=n;i++)
+    {
+        if(primes[i]==true) cout<<i<<endl;
+    }
+    return 0;
+}
