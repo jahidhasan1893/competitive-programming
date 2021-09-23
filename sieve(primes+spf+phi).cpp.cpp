1d8cf586b1091cfaec85ca71c8aca3073e2a7211
jahidhasan1893 <78349772+jahidhasan1893@users.noreply.github.com>
2021-09-23 16:21:57 +0600

    Update and rename linear_sieve.cpp to sieve(primes+spf+phi).cpp.cpp

--- sieve(primes+spf+phi).cpp.cpp
+++ sieve(primes+spf+phi).cpp.cpp
@@ -1,9 +1,10 @@
 //printing all primes upto n +
-//printing shortest prime factor(spf) of all number upto n
+//printing shortest prime factor(spf) of all number upto n+
+//printing all phi functions upto n
 
 #include<bits/stdc++.h>
 using namespace std;
-void sieve_of_eratosthenes(bool is_prime[],int spf[],vector<int>&primes,int n)
+void sieve(bool is_prime[],int spf[],vector<int>&primes,int phi[],int n)
 {
     for(int i=2;i<=n;i++)
     {
@@ -11,14 +12,17 @@ void sieve_of_eratosthenes(bool is_prime[],int spf[],vector<int>&primes,int n)
         {
             primes.push_back(i);
             spf[i]=i;
-            for(int j=i*i;j<=n;j+=i)
+            for(int j=i;j<=n;j+=i)
             {
                 is_prime[j]=false;
                 if(spf[j]==0) spf[j]=i;
+                phi[j]-=phi[j]/i;
             }
         }
     }
 }
+
+
 int main()
 {
     int n;
@@ -26,20 +30,31 @@ int main()
     bool is_prime[n+1];
     int spf[n+1];
     vector<int>primes;
+    int phi[n+1];
     memset(is_prime,true,sizeof(is_prime));
     memset(spf,0,sizeof(spf));
+    for(int i=0;i<=n;i++)
+    {
+        phi[i]=i;
+    }
     is_prime[0]=is_prime[1]=false;
-    sieve_of_eratosthenes(is_prime,spf,primes,n);
+    sieve(is_prime,spf,primes,phi,n);
     cout<<"primes upto n ->"<<endl;
     for(int i=0;i<primes.size();i++)
     {
-        cout<<primes[i]<<endl;
+        cout<<primes[i]<<" ";
     }
+    cout<<endl;
     cout<<"smallest primes factors(spf) upto n ->"<<endl;
     for(int i=1;i<=n;i++)
     {
         if(spf[i]==0) cout<<i<<" -> No prime factors"<<endl;
         else cout<<i<<" -> "<<spf[i]<<endl;
+    }
+    cout<<"phi functions upto n ->"<<endl;
+    for(int i=1;i<=n;i++)
+    {
+        cout<<i<<" -> "<<phi[i]<<endl;
     }
     return 0;
 }
