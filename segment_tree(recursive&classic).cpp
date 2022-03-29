377e073a958fddb4babea48011e7ec8b8321c145
jahidhasan1893 <78349772+jahidhasan1893@users.noreply.github.com>
2022-03-29 16:48:27 +0600

    Update segment_tree(recursive&classic).cpp

--- segment_tree(recursive&classic).cpp
+++ segment_tree(recursive&classic).cpp
@@ -14,6 +14,20 @@ void build(int a[],int node,int st,int e)
     build(a,node*2+1,mid+1,e);
     s[node]=s[2*node]+s[(2*node)+1];
 }
+void update(int a[],int node,int st,int e,int pos,int val)
+{
+    if(pos<st || pos>e) return;
+    if(st==e)
+    {
+        a[pos]=val;
+        s[node]=val;
+        return;
+    }
+    int mid=(st+e)/2;
+    if(pos<=mid) update(a,2*node,st,mid,pos,val);
+    else update(a,2*node+1,mid+1,e,pos,val);
+    s[node]=s[2*node]+s[2*node+1];
+}
 int query(int a[],int node,int st,int e,int l,int r)
 {
     if(st>r || l>e || st>e) return 0;
@@ -28,16 +42,27 @@ int main()
     int n,q;
     cin>>n>>q;
     int a[n];
-    for(int i=0;i<n;i++)
+    for(int i=0; i<n; i++)
     {
         cin>>a[i];
     }
     build(a,1,0,n-1);
-    for(int i=0;i<q;i++)
+    for(int i=0; i<q; i++)
     {
-        int l,r;
-        cin>>l>>r;
-        cout<<query(a,1,0,n-1,l-1,r-1)<<endl;;
+        char qt;
+        cin>>qt;
+        if(qt=='q')
+        {
+            int l,r;
+            cin>>l>>r;
+            cout<<query(a,1,0,n-1,l-1,r-1)<<endl;
+        }
+        else if(qt=='u')
+        {
+            int pos,val;
+            cin>>pos>>val;
+            update(a,1,0,n-1,pos-1,val);
+        }
     }
     return 0;
 }
